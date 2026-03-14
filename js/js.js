1e407d82c1f0803b4481bf747a8f273383fa5413
maksym1177 <maksikdobus@gmail.com>
2026-03-14 22:51:55 +0200

    +login\register UI

--- js/js.js
+++ js/js.js
@@ -2,7 +2,7 @@
 const moreTrBtn = document.getElementById("more-tr-btn");
 const gallery = document.getElementsByClassName("grid-photos")[0];
 
-let k = 0;
+let k = 1;
 const formats = ["png","jpg","jpeg","webp"];
 
 moreTrBtn.addEventListener("click" , ()=>{
@@ -31,6 +31,8 @@ moreTrBtn.addEventListener("click" , ()=>{
 
     k += 4;
 });
+
+/*----------------------------------page changer--------------*/
 const links = document.querySelectorAll(".mid-header a");
 const pages = document.querySelectorAll("[class^='page']");
 
@@ -48,4 +50,60 @@ links.forEach((link, index) => {
 
         link.style.color = "var(--dark-green)";
     });
+});
+/*----------------------------------login\register form --------------*/
+const loginForm = document.getElementById("login-form");
+const regForm = document.getElementById("register-form");
+const loginLink = document.getElementById("login-link");
+const regLink = document.getElementById("register-link");
+loginLink.addEventListener("click",()=>{
+
+    regForm.style.display = "none";
+    loginForm.style.display = "flex";
+    
+})
+regLink.addEventListener("click",()=>{
+
+    loginForm.style.display = "none";
+    regForm.style.display = "flex";
+})
+/*----------------------------------login\register form password hide\show --------------*/
+const loginEye = document.getElementById("login-eye");
+const loginPass = document.getElementById("login-pass");
+
+loginEye.addEventListener("click", () => {
+    if(loginPass.type === "password") {
+        loginPass.type = "text";
+        loginEye.classList.remove("closed");
+    } else {
+        loginPass.type = "password";
+        loginEye.classList.add("closed");
+    }
+});
+
+const regEye = document.getElementById("reg-eye");
+const regPass = document.getElementById("register-pass");
+regEye.addEventListener("click", () => {
+    if(regPass.type === "password") {
+        regPass.type = "text";
+        regEye.classList.remove("closed");
+    } else {
+        regPass.type = "password";
+        regEye.classList.add("closed");
+    }
+});
+
+/*----------------------------------profile menu --------------*/
+const pLogin = document.getElementById("profile-login");
+const formDiv = document.getElementsByClassName("form-div")[0];
+
+pLogin.onclick = (e) => {
+    e.stopPropagation(); 
+    formDiv.style.display = "block";
+};
+
+document.addEventListener('click', (e) => {
+    if (formDiv.style.display === "block" && !formDiv.contains(e.target)) {
+        formDiv.style.display = 'none';
+    }
 });
